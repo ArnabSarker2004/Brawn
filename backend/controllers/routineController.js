25078dfdf58d6b496a9a4141828c828de1c872fa
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-12-14 16:13:17 -0500

    routine controller now allows for user to be searched with the workout routine

--- backend/controllers/routineController.js
+++ backend/controllers/routineController.js
@@ -2,87 +2,100 @@ const Routine = require('../models/routineModel');
 const mongoose = require('mongoose');
 
 const getRoutines = async (req, res) => {
-const routines = await Routine.find({}).sort({ createdAt: -1 });
-res.status(200).json(routines);
+    const userID = req.user.id;
+    const routines = await Routine.find({user: userID}).sort({ createdAt: -1 }); 
+    res.status(200).json(routines);
 };
 
 const getRoutine = async (req, res) => {
-const { id } = req.params;
+    const { id } = req.params;
+    const userID = req.user.id;
 
-if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: 'No such routine' });
-}
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
 
-const routine = await Routine.findById(id);
+    const routine = await Routine.findOne({_id: id, user: userID});
 
-if (!routine) {
-    return res.status(404).json({ error: 'No such routine' });
-}
+    if (!routine) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
 
-res.status(200).json(routine);
+    res.status(200).json(routine);
 };
 
 const createRoutine = async (req, res) => {
-const { name, workouts } = req.body;
+    const userID = req.user.id;
+    const { name, workouts } = req.body;
 
-let emptyFields = [];
+    let emptyFields = [];
 
-if (!name) {
-    emptyFields.push('name');
-}
+    if (!name) {
+        emptyFields.push('name');
+    }
 
-if (workouts && (!Array.isArray(workouts))) {
-    emptyFields.push('workouts');
-}
+    if (workouts && (!Array.isArray(workouts))) {
+        emptyFields.push('workouts');
+    }
 
-if (emptyFields.length > 0) {
-    return res.status(400).json({ error: 'Please fill in all the fields', emptyFields });
-}
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields });
+    }
 
-try {
-    const routine = await Routine.create({ name, workouts: workouts || [] });
-    res.status(200).json(routine);
-} catch (error) {
-    res.status(400).json({ error: error.message });
-}
+    try {
+        const routine = await Routine.create({ 
+            name, 
+            workouts: workouts || [],
+            user: userID 
+        });
+        res.status(200).json(routine);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 const deleteRoutine = async (req, res) => {
-const { id } = req.params;
+    const { id } = req.params;
+    const userID = req.user.id;
 
-if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: 'No such routine' });
-}
 
-const routine = await Routine.findOneAndDelete({ _id: id });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
 
-if (!routine) {
-    return res.status(404).json({ error: 'No such routine' });
-}
+    const routine = await Routine.findOneAndDelete({ _id: id, user: userID });
 
-res.status(200).json(routine);
+    if (!routine) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
+
+    res.status(200).json(routine);
 };
 
 const updateRoutine = async (req, res) => {
-const { id } = req.params;
+    const userID = req.user.id;
+    const { id } = req.params;
 
-if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: 'No such routine' });
-}
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
 
-const routine = await Routine.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true });
+    const routine = await Routine.findOneAndUpdate(
+        { _id: id, user: userID },
+        { ...req.body }, 
+        { new: true });
 
-if (!routine) {
-    return res.status(404).json({ error: 'No such routine' });
-}
+    if (!routine) {
+        return res.status(404).json({ error: 'No such routine' });
+    }
 
-res.status(200).json(routine);
+    res.status(200).json(routine);
 };
 
 module.exports = {
-getRoutines,
-getRoutine,
-createRoutine,
-deleteRoutine,
-updateRoutine
+    getRoutines,
+    getRoutine,
+    createRoutine,
+    deleteRoutine,
+    updateRoutine
 };
