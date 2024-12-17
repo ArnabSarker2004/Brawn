c41bb5a3a5bf725ec49d66ff777f4b56ede260f3
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-12-16 20:38:49 -0500

    fixed all the css for the routines

--- frontend/src/pages/Routines.js
+++ frontend/src/pages/Routines.js
@@ -4,70 +4,68 @@ import RoutineFormModal from '../custom-components/Modals/RoutineFormModal';
 import { useRoutinesContext } from '../hooks/useRoutinesContext';
 import {Button} from '../components/ui/button';
 const Routines = () => {
-  const token = localStorage.getItem('token');
-  const { routines, dispatch } = useRoutinesContext();
-  const [showAddModal, setShowAddModal] = useState(false);
+    const token = localStorage.getItem('token');
+    const { routines, dispatch } = useRoutinesContext();
+    const [showAddModal, setShowAddModal] = useState(false);
 
-  useEffect(() => {
-    const fetchRoutines = async () => {
-      const response = await fetch('/api/routines',
-        { method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`}
+    useEffect(() => {
+        const fetchRoutines = async () => {
+        const response = await fetch('/api/routines',
+            { method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`}
+            }
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+            dispatch({ type: 'SET_ROUTINES', payload: data });
+        } else {
+            console.error('Failed to fetch routines');
         }
-      );
-      const data = await response.json();
-
-      if (response.ok) {
-        dispatch({ type: 'SET_ROUTINES', payload: data });
-      } else {
-        console.error('Failed to fetch routines');
-      }
     };
 
-    fetchRoutines();
-  }, [dispatch]);
+        fetchRoutines();
+    }, [dispatch]);
 
-  const handleDeleteRoutine = async (id) => {
-    const response = await fetch(`/api/routines/${id}`, { 
-      method: 'DELETE', 
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }  
-    });
+    const handleDeleteRoutine = async (id) => {
+        const response = await fetch(`/api/routines/${id}`, { 
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }  
+        });
 
-    if (response.ok) {
-      const deletedRoutine = await response.json();
-      dispatch({ type: 'DELETE_ROUTINE', payload: deletedRoutine });  
-    } else {
-      console.error('Failed to delete routine');
-    }
-  };
+        if (response.ok) {
+            const deletedRoutine = await response.json();
+            dispatch({ type: 'DELETE_ROUTINE', payload: deletedRoutine });  
+        } else {
+            console.error('Failed to delete routine');
+        }
+    };
 
-  return (
-    <div className="routine-grid">
-      <div className="add-routine-row">
-        <Button variant="default" onClick={() => setShowAddModal(true)}>
-          Add Routine
-        </Button>
-      </div>
+    return (
 
-      <div className="routine-list">
-        {routines && routines.map((routine) => (
-          <Routine
-            key={routine._id}
-            routine={routine}
-            onDelete={handleDeleteRoutine} 
-          />
-        ))}
-      </div>
-      
-      {showAddModal && <RoutineFormModal onClose={() => setShowAddModal(false)} />}
-    </div>
-    
-  );
+        <div className="routine-grid">
+            <div className="add-routine-row">
+                <Button variant="default" className= "w-full text-lg mb-4" onClick={() => setShowAddModal(true)}>
+                    Add Routine
+                </Button>
+            </div>
+            <div className="routine-list">
+                {routines && routines.map((routine) => (
+                <Routine
+                    key={routine._id}
+                    routine={routine}
+                    onDelete={handleDeleteRoutine} 
+                />
+                ))}
+            </div>
+            {showAddModal && <RoutineFormModal onClose={() => setShowAddModal(false)} />}
+        </div>
+    );
 };
 
 export default Routines;
