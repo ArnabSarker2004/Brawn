0b9038a17ab6f92c6d03fcc0f66ad4a912f990fa
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-12-28 21:25:17 -0500

    longest day streak algorithm implementation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,59 @@
 const User = require('../models/User');
 const Routine = require('../models/routineModel');
+
 const getBodyInfo = async (req, res) =>{
     const userID = req.body.username;
     const ID = req.user.id;
     const routines = await Routine.find({user: ID}).sort({ createdAt: -1 }); 
     let totalWorkouts = 0;
+    let completitionStats = [];
     routines.forEach(
         (routine) => {
-            totalWorkouts += routine.completionStats.length
+            totalWorkouts += routine.completionStats.length;
+            if (routine.completionStats.length !=0) completitionStats.push(routine.completionStats);
         }
     );
 
+    const uniqueDays = Array.from(new Set(completitionStats.flat(2).map(entry => entry.date.split("T")[0])));
+
+    
     const body = await User.findOne({username: userID});
     if (!body) return res.status(404).json({error: 'Body doesn\'t exist'});
+    const dayPriorExists = (isoSet, isoString) =>{
+        const givenDate = new Date(isoString);
+        const dayPrior = new Date(givenDate);
+        dayPrior.setDate(givenDate.getDate()-1);
+        const priorDay = dayPrior.toISOString().split("T")[0];
+        return Array.from(isoSet).some((_) => _.startsWith(priorDay));
+        
+    };
 
+    const dayAfterExists = (isoSet, isoString) =>{
+        const givenDate = new Date(isoString);
+        const dayAfter = new Date(givenDate);
+        dayAfter.setDate(givenDate.getDate()+1);
+        const afterDay = dayAfter.toISOString().split("T")[0];
+        return Array.from(isoSet).some((_)=> _.startsWith(afterDay));
+    };
+    
+    // did this using neetcode video on leetcode medium, longest consecutive sequence
+    let longestStreak = 0;
+    uniqueDays.forEach((_) =>{
+        if(!dayPriorExists(uniqueDays, _)){
+            let length = 0;
+            let currentDay = _;
+            while (dayAfterExists(uniqueDays, currentDay)){
+                console.log(length);
+                length++;
+                const currentDate = new Date(currentDay);
+                currentDate.setDate(currentDate.getDate()+1);
+                currentDay = currentDate.toISOString().split("T")[0];
+            }
+            length ++;
+            longestStreak = Math.max(longestStreak, length);
+        }
+    });
+    
     res.status(200).json({
         Name: body.Name,
         Email:body.Email,
